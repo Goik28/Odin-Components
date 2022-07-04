ceb972fcc2ebad337fbf527632c36e8cbd09856f
Goik <sbabinski@outlook.com>
2022-07-04 20:57:31 -0300

    Create project files and install npm modules
    Create .js/.html/.css files for each module.
    Install and configure webpack.
    Install and configure eslint and prettier.
    Create README.MD.

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Components',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+              interpolation: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
