4e615be22f3e97f5b7cc0b0008c2a6bfabbef76d
Goik <sbabinski@outlook.com>
2022-07-07 15:46:14 -0300

    Redesign the drop-down-menu
    Change the position of the drop-down-menu items to absolute.
    Fix their width to be the same size.
    Lock the menu color when active.

--- src/Components/drop-down-menu/drop-down-menu.js
+++ src/Components/drop-down-menu/drop-down-menu.js
@@ -1,22 +1,31 @@
 import html from "./drop-down-menu.html";
 import "./drop-down-menu.css";
 
-export function createDropDownMenu(){
-const main = document.createElement("div");
-main.innerHTML = html;
-const menu = main.getElementsByClassName("menu")[0];
-menu.addEventListener("mouseenter", turnVisible);
-menu.addEventListener("mouseleave", turnInvisible);
-    
-return main;
+export function createDropDownMenu() {
+  const main = document.createElement("div");
+  main.className = "header";
+  main.textContent = "Drop-Down-Menu";
+  main.appendChild(createList());
+
+  main.addEventListener("mouseenter", turnVisible);
+  main.addEventListener("mouseleave", turnInvisible);
+
+  return main;
 }
 
+function createList() {
+  const ol = document.createElement("ol");
+  ol.innerHTML = html;
+  ol.classList.add("list-container");
+  return ol;
+}
 
 function turnVisible() {
   const menuItems = Array.from(document.getElementsByClassName("menu-item"));
   menuItems.forEach((element) => {
     element.classList.add("menu-item-show");
   });
+  document.getElementsByClassName("header")[0].classList.add("header-lock");
 }
 
 function turnInvisible() {
@@ -24,4 +33,5 @@ function turnInvisible() {
   menuItems.forEach((element) => {
     element.classList.remove("menu-item-show");
   });
+  document.getElementsByClassName("header")[0].classList.remove("header-lock");
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import { createDropDownMenu } from "./Components/drop-down-menu/drop-down-menu.js";
 
 document.body.appendChild(createDropDownMenu());
+const separator1 = document.createElement("h1");
+separator1.textContent = "element that shouldnt be pushed.";
+document.body.appendChild(separator1);
