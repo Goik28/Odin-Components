c6205df28fda1be14abf8c358ca2a75361ae6bae
Goik <sbabinski@outlook.com>
2022-07-13 19:15:45 -0300

    Select current image on radio click
    Create a function to change the displayed image on radio click.
    Add id field for the image tags.
    Fix the value on the radio button tags.

--- src/Components/image-slider/image-slider.js
+++ src/Components/image-slider/image-slider.js
@@ -5,6 +5,24 @@ export function createImageSlider() {
   const main = document.createElement("ol");
   main.innerHTML = html;
   main.classList.add("image-slider");
-
+  selectImage(main);
   return main;
+}
+
+function selectImage(main) {
+  const inputs = Array.from(
+    main
+      .getElementsByClassName("image-selector")[0]
+      .getElementsByTagName("input")
+  );
+  const images = main.getElementsByClassName("image-item");
+  inputs.forEach((element, index) => {
+    element.addEventListener("click", () => {
+      element.checked = true;
+      document
+        .getElementsByClassName("image-item-selected")[0]
+        .classList.remove("image-item-selected");
+      images[index].classList.add("image-item-selected");
+    });
+  });
 }
