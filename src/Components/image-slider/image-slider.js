68502942f6a353b539e7a68ed27f0c36334b9648
Goik <sbabinski@outlook.com>
2022-07-14 17:25:46 -0300

    Crate function for next and previous image
    Create function to go to the next image on click.
    Create function to go to the previous image on click.

--- src/Components/image-slider/image-slider.js
+++ src/Components/image-slider/image-slider.js
@@ -6,6 +6,8 @@ export function createImageSlider() {
   main.innerHTML = html;
   main.classList.add("image-slider");
   selectImage(main);
+  nextImage(main);
+  previousImage(main);
   return main;
 }
 
@@ -25,4 +27,60 @@ function selectImage(main) {
       images[index].classList.add("image-item-selected");
     });
   });
+}
+
+function nextImage(main) {
+  const inputs = Array.from(
+    main
+      .getElementsByClassName("image-selector")[0]
+      .getElementsByTagName("input")
+  );
+  const images = main.getElementsByClassName("image-item");
+  main.getElementsByClassName("next-item")[0].addEventListener("click", () => {
+    const activeIndex = inputs.findIndex((element) => {
+      return element.checked == true;
+    });
+    if (activeIndex == inputs.length - 1) {
+      inputs[0].checked = true;
+      document
+        .getElementsByClassName("image-item-selected")[0]
+        .classList.remove("image-item-selected");
+      images[0].classList.add("image-item-selected");
+    } else {
+      inputs[activeIndex + 1].checked = true;
+      document
+        .getElementsByClassName("image-item-selected")[0]
+        .classList.remove("image-item-selected");
+      images[activeIndex + 1].classList.add("image-item-selected");
+    }
+  });
+}
+
+function previousImage(main) {
+  const inputs = Array.from(
+    main
+      .getElementsByClassName("image-selector")[0]
+      .getElementsByTagName("input")
+  );
+  const images = main.getElementsByClassName("image-item");
+  main
+    .getElementsByClassName("previous-item")[0]
+    .addEventListener("click", () => {
+      const activeIndex = inputs.findIndex((element) => {
+        return element.checked == true;
+      });
+      if (activeIndex == 0) {
+        inputs[inputs.length - 1].checked = true;
+        document
+          .getElementsByClassName("image-item-selected")[0]
+          .classList.remove("image-item-selected");
+        images[inputs.length - 1].classList.add("image-item-selected");
+      } else {
+        inputs[activeIndex - 1].checked = true;
+        document
+          .getElementsByClassName("image-item-selected")[0]
+          .classList.remove("image-item-selected");
+        images[activeIndex - 1].classList.add("image-item-selected");
+      }
+    });
 }
